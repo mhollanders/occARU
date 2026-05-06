e0ccbfa3accada6e2ff81b09c4170b44a0b1a0a7
Mathijs Hollanders <matthijs.hollanders@gmail.com>
2026-05-06 15:51:37 +1000

    try S3 again

--- R/occARU_fit-methods.R
+++ R/occARU_fit-methods.R
@@ -33,7 +33,8 @@
 #' @return A ggplot object with posterior predictive rootograms faceted by
 #'   species which can be modified using the \pkg{ggplot2} package.
 #' @importFrom bayesplot pp_check
-#' @exportS3Method bayesplot::pp_check
+#' @export pp_check
+#' @export
 pp_check.occARU_fit <- function(
   object,
   level = c("Q", "y"),
